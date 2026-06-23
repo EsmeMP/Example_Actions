5bef8b28269145da786da09d52bc1081a357bffc
Esme Munoz <esmeralda.padilla.com@gmail.com>
2026-06-23 13:27:54 -0600

    08 Tratando de correr con js II

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-import os 
+// import os 
 
 // name = os.getenv("USERNAME")
-console.log("Hola desde jaibascript")
+for(let i = 1; i < 10; i++){
+    console.log('♞'.repeat(i))
+}
+
+console.log("Hola desde jaibascript :3")
